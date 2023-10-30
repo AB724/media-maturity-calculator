597070656705ddd800170b7071fdc9cfb2f89bee
Abhinay Agarwal <ab@Abhinays-MacBook-Pro-2.local>
2023-10-29 17:39:26 -0700

    Description of the changes made

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-function calculateScore() {
+document.getElementById("quizForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
     let totalScore = 0;
+    const formData = new FormData(event.target);
 
-    // Add up the scores from all questions.
-    for (let i = 1; i <= 10; i++) {
-        totalScore += parseInt(document.getElementById(`q${i}`).value);
+    // Sum up the scores from the form data
+    for (let value of formData.values()) {
+        totalScore += parseInt(value);
     }
 
-    // Display the total score.
-    document.getElementById("score").innerText = totalScore;
-
-    // Give recommendations based on the score.
-    let recommendation = "";
-
-    if (totalScore <= 20) {
-        recommendation = "Recommendation for lower scores. Try out Cloudinary's basic features!";
-    } else if (totalScore <= 40) {
-        recommendation = "Recommendation for mid-range scores. Explore advanced features of Cloudinary!";
-    } else {
-        recommendation = "You're advanced! Dive deeper into Cloudinary's expert tools!";
-    }
-
-    document.getElementById("recommendation").innerText = recommendation;
-    document.getElementById("results").style.display = "block";
-}
+    // Display the result
+    document.getElementById("score").textContent = totalScore;
+    document.getElementById("result").hidden = false;
+});
